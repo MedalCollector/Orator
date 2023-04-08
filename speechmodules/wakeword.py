c2220dcf8cd91cae9fbe0b8b496ccf0b8e7ab066
zhangchenyu <zhangchenyu@192.168.0.112>
2023-04-08 11:30:49 +0800

    Add wakeword module to Orator

---
+++ speechmodules/wakeword.py
@@ -0,0 +1,41 @@
+import pvporcupine
+import pyaudio
+import struct
+PICOVOICE_API_KEY = ""
+keyword_path = '*.ppn'
+
+
+class PicoWakeWord:
+    def __init__(self, PICOVOICE_API_KEY, keyword_path):
+        self.PICOVOICE_API_KEY = PICOVOICE_API_KEY
+        self.keyword_path = keyword_path
+        self.porcupine = pvporcupine.create(
+            access_key=self.PICOVOICE_API_KEY,
+            keyword_paths=[self.keyword_path]
+        )
+        self.myaudio = pyaudio.PyAudio()
+        self.stream = self.myaudio.open(
+            input_device_index=0,
+            rate=self.porcupine.sample_rate,
+            channels=1,
+            format=pyaudio.paInt16,
+            input=True,
+            frames_per_buffer=self.porcupine.frame_length
+        )
+
+    def detect_wake_word(self):
+        audio_obj = self.stream.read(self.porcupine.frame_length, exception_on_overflow=False)
+        audio_obj_unpacked = struct.unpack_from("h" * self.porcupine.frame_length, audio_obj)
+        keyword_idx = self.porcupine.process(audio_obj_unpacked)
+        return keyword_idx
+
+
+if __name__ == '__main__':
+    picowakeword = PicoWakeWord(PICOVOICE_API_KEY, keyword_path)
+    while True:
+        audio_obj = picowakeword.stream.read(picowakeword.porcupine.frame_length, exception_on_overflow=False)
+        audio_obj_unpacked = struct.unpack_from("h" * picowakeword.porcupine.frame_length, audio_obj)
+
+        keyword_idx = picowakeword.porcupine.process(audio_obj_unpacked)
+        if keyword_idx >=0:
+            print("我听到了！")
